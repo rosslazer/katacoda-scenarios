3673151661c2b29515f1f1eb38033877f4677832
ross <ross@observeinc.com>
2020-05-29 11:13:19 -0700

    Changed init

--- test5/env-init.sh
+++ test5/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "while [ \`minikube status &>/dev/null; echo \$?\` -ne 0 ]; do sleep 1; done && kubectl apply -f deploy.yaml && echo done >> /opt/katacoda-completed"
+ssh root@host01 "kubectl apply -f deploy.yaml"
